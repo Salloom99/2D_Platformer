b979afbc46fbb53a0df7cd36ca6f552fa6d2d483
Salloom <salem.domani@gmail.com>
2021-09-05 19:12:48 +0300

    new Dummy animation

--- Assets/Scripts/Enemies/CombatTest.cs
+++ Assets/Scripts/Enemies/CombatTest.cs
@@ -9,18 +9,31 @@ public class CombatTest : MonoBehaviour,IDamageable
 
     private Animator anim;
 
+    private float wiggleTime = 60f;
+
+    private float startTimer;
+
     public void Damage(float Damage)
     {
         Debug.Log(Damage + "amount of damage taken");
 
         Instantiate(hitParticles,transform.position,Quaternion.Euler(0f,0f,Random.Range(0f,360f)));
-        anim.SetTrigger("damage");
-
-        Destroy(gameObject);
+        anim.SetBool("damage",true);
+        startTimer = Time.time;;
+        //Destroy(gameObject);
     }
 
     private void Awake() 
     {
         anim = GetComponent<Animator>();
+    }
+
+    private void Update() {
+
+        if( Time.time > startTimer + wiggleTime)
+        {
+            anim.SetBool("damage",false);
+            Debug.Log("stopped");
+        }
     }
 }
